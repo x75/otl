1c1cfb5799f1ef0d5defdb59fc07a540cd71a58b
Harold Soh <haroldsoh@imperial.ac.uk>
2012-05-30 14:31:08 +0100

    forgotten SOGP_MATLAB. moving it to it's own repository

--- SOGP_MATLAB/getKStar.m
+++
@@ -1,6 +0,0 @@
-function [ kstar ] = getKStar( x, PSOGP )
-%GETKSTAR Summary of this function goes here
-%   Detailed explanation goes here
-    kstar = PSOGP.covf(x, x, PSOGP.covf_params); 
-end
-
--- SOGP_MATLAB/kern_gaussian.m
+++
@@ -1,8 +0,0 @@
-function [ kval ] = kern_gaussian( x, y, params )
-%RKERN_GAUSSIAN Summary of this function goes here
-%   Detailed explanation goes here
-    sigmai = params(1);
-    kval = exp(- norm((x - y) )^2/(2*sigmai^2));
-
-end
-
